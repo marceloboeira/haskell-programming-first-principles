04e6afa3c46a1fdd3171b8977c9844b381d8e333
Patrick <patbecich@gmail.com>
2017-01-17 19:27:52 -0800

    Type argument practice

--- chapter5Types.hs
+++ chapter5Types.hs
@@ -50,3 +50,32 @@ uncurry' f (a, b) = f a b
 typicalUncurriedFunction :: (Integer , Bool) -> Integer
 typicalUncurriedFunction = uncurry' typicalCurriedFunction
 
+-- Exercises: Type Arguments (p. 162)
+
+--1) f x :: Char -> Char -> Char
+--2) g :: a -> b -> c -> b
+--   :t g x y z :: Char when x :: Int, y :: Char, z :: String
+
+-- 3) h :: (Num a, Num b) => a -> b -> b
+--    :t h 1.0 2 :: Integer
+
+-- 4) h :: (Num a, Num b) => a -> b -> b
+--    :t h 1 (5.5 :: Double) :: Double
+
+-- 5) jackal :: (Ord a, Eq b) => a -> b -> a
+--    :t jackal "keyboard" "has the word jackal in it" :: [Char]
+
+-- 6) jackal :: (Ord a, Eq b) => a -> b -> a
+--    :t jackal "keyboard" :: (Eq b) => b -> [Char]
+
+-- 7) kessel :: (Ord a, Num b) => a -> b -> a
+--    :t kessel 1 2 :: (Ord a, Num a) => a
+
+-- 8) kessel :: (Ord a, Num b) => a -> b -> a
+--    :t kessel 1 (2 :: Integer) :: (Ord a, Num a) => a
+
+-- 9) kessel :: (Ord a, Num b) => a -> b -> a
+--    :t kessel (1 :: Integer) 2 :: Integer
+
+-- 5.6 Polymorphism
+
