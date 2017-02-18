df4bd73c212985a60cb8ca1739116550beba4aed
Patrick <patbecich@gmail.com>
2017-02-18 15:23:44 -0800

    Hutton's Razor exercise

--- chapter11Exercises.hs
+++ chapter11Exercises.hs
@@ -216,4 +216,12 @@ messageTaps message = totalTaps (reverseTaps message)
 
 data Expr = Lit Integer | Add Expr Expr
 
---eval :: Expr -> Integer
+eval :: Expr -> Integer
+eval (Lit int) = int
+eval (Add a b) = (eval a) + (eval b)
+
+-- 2
+
+printExpr :: Expr -> String
+printExpr (Lit int) = show int
+printExpr (Add a b) = (printExpr a) ++ " + " ++ (printExpr b)
